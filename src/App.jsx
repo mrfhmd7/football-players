1026b2293cd508534a4fd493e59b02fc2185bbd9
mrfhmd7 <mrf.hmd7@gmail.com>
2024-01-13 21:53:29 +0600

    header file added and fixed eslint

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css'
+import Header from './components/Header/Header'
 
 function App() {
   return (
     <>
-      <h1>this is my website.</h1>
+      <Header></Header>
     </>
   )
 }
