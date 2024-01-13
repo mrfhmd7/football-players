e214749dd782f1416d4cb4ee3d53202f832a17a8
mrfhmd7 <mrf.hmd7@gmail.com>
2024-01-13 22:23:10 +0600

    header fixed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 
 const Header = () => {
      return (
-          <div>
-               <h1>This is header</h1>
-          </div>
+          <nav className='bg-indigo-500 text-white h-20 flex items-center justify-between pl-24 pr-24 rounded-md  font-medium text-lg'>
+               <h4>Player Market</h4>
+               <div>
+                    <a className='ml-10' href="/home">Home</a>
+                    <a className='ml-10' href="/review">Review</a>
+                    <a className='ml-10' href="/about">About</a>
+               </div>
+          </nav>
      );
 };
 
