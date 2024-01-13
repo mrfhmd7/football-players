a25895091d92ec514d4866dd75d18f55e765262c
mrfhmd7 <mrf.hmd7@gmail.com>
2024-01-13 22:48:05 +0600

    Market, Player & Cart component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Header from './components/Header/Header'
+import Market from './components/Market/Market'
 
 function App() {
   return (
     <>
       <Header></Header>
+      <Market></Market>
     </>
   )
 }
---
+++ src/components/Market/Market.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Market = () => {
+     return (
+          <div>
+               <h2>This is market component.</h2>
+          </div>
+     );
+};
+
+export default Market;
